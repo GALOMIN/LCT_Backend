d18e6d9d60d8a3579cb79feeff6f26f5661a4781
GALOMIN <123244294+GALOMIN@users.noreply.github.com>
2023-05-28 23:17:16 +0300

    Add files via upload

---
+++ taskmanager/main/templates/main/main.js
@@ -0,0 +1,23 @@
+function nextCardStep1() {
+    document.getElementById('content').innerHTML = document.getElementById('tmpl_test').innerHTML
+}
+
+function nextCardStep2() {
+    document.getElementById('content').innerHTML = document.getElementById('tmpl_champ').innerHTML
+}
+
+function nextCardStep3() {
+    document.getElementById('content').innerHTML = document.getElementById('tmpl_student').innerHTML
+}
+
+function nextCardStep4() {
+    document.getElementById('content').innerHTML = document.getElementById('tmpl_school').innerHTML
+}
+
+function showProfile() {
+    document.getElementById('content').innerHTML = document.getElementById('tmpl_profile').innerHTML
+}
+
+function showMain() {
+    document.getElementById('content').innerHTML = document.getElementById('tmpl_school').innerHTML
+}
